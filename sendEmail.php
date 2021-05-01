b58da8ab0814a5f0d13a9b8712b8f50887574b95
Maximilian Faust <BsHr_Faust@web.de>
2021-05-01 23:54:37 +0200

    correction of spelling on about page and setting up of correct email tranfer format

--- sendEmail.php
+++ sendEmail.php
@@ -8,6 +8,14 @@ if($_POST["name"] == "" || $_POST["email"] == "" || $_POST["message"] == ""){
 // Check if the "Sender's Email" input field is filled out
 $email=$_POST['email'];
 $from = "booking@lomi5.de";
+$name = $_POST['name'];
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
 // Sanitize E-mail Address
 $email =filter_var($email, FILTER_SANITIZE_EMAIL);
 // Validate E-mail Address
@@ -16,14 +24,19 @@ if (!$email){
 echo "Invalid Sender's Email";
 }
 else{
-$subject = $_POST['sub'];
-$message = $_POST['message'];
-$headers = 'From:'. $email2 . "rn"; // Sender's Email
-$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
+$subject = "Neue Nutzeranfrage von ".$name; 
+$mailText = $_POST['message'];
+$message = '<html><body style="background-color:#F9F5EC;>';
+$message .= '<h1 style="color:#262626;">Hallo Henriette!</h1>';
+$message .= '<h3 style="color:#262626;">'. $name . ' Hat eine Anfrage.</h3';
+$message .= '<p style="color:#262626;">Inhalt der Nachricht:</p';
+$message .= '<p style="color:#262626;">' . $mailText . '</p';
+$message .= '<p style="color:#262626;">Emailaddresse vom Kunden:' . $email . '</p';
+$message .= '</body></html>';
 // Message lines should not exceed 70 characters (PHP rule), so wrap it
 $message = wordwrap($message, 70);
 // Send Mail By PHP Mail Function
-if (!mail("info@lomi5.de", "Neue Nutzeranfrage", $message, $headers)) {
+if (!mail("info@lomi5.de", $subject, $message, $headers)) {
     header("Location: https://www.lomi5.de/error.html");
     die();
  }
